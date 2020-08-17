03b211b0c0fb595d11f9db9dfd03d02eed2f2eac
emmanuerl <emekaemmanuel045@gmail.com>
2020-08-17 01:21:58 +0100

    route error handlers

--- loaders/routes.js
+++ loaders/routes.js
@@ -2,43 +2,34 @@ import cors from "cors";
 import bodyParser from "body-parser";
 
 /**
- * Configures and loads all routes
- * 
- * @param {Object} app express app instance 
- * @param {Config} config  config object
+ * Registers all api routes with the express app.
+ * @param  {object} config an exoress app instalce
+ * @return {Promise}
  */
+export default function loadRoutes(app, config) {
+  return new Promise((resolve, reject) => {
+    app.use(cors());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
 
-const loader = (app,config) => 
-    new Promise(resolve => { 
-      app.use(cors);
-
-      app.get('/',(req,res) => {
-        res.send("mad oo from: "+ __dirname)
-        })
-
-      resolve()
+    app.get('/',(req,res,next)=>{
+      //res.send('hit');
+      next({
+        status: 500,
+        message: "error dey o"
+      })
     })
-    
-    // app.use(bodyParser.json());
-    // app.use(bodyParser.urlencoded({
-    //   extended: false
-    // }))
 
-    // app.use((req,res,next)=> {
-    //   console.log(req.url);
-    //   next();
-    // })
+    // error handling routes
+    app.use((req, res, next) => {
+      res.status(404).send('route not found');
+     })
 
-    // app.use(({ status, message}, req, res, next) =>{
-    //   // handle server errors
-    //   if(!status || !message){
-    //     status = 500;
-    //     message = "Internal Server Error"
-    //   }
-    //   console.log(status);
-    //   res.status(status).json({ message })
-    // })
+     app.use((error,req, res, next) => {
+      console.error(error.stack);
+      res.status(error.status).send(error.message);
+     })
 
-    // console.log('ended');
-
-export default loader
+    resolve();
+  });
+}
